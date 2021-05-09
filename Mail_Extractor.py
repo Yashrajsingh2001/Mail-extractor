e91b11658eb843b6d6c78c902b48b029d0200a91
Yashraj Singh Boparai <53632623+Yashrajsingh2001@users.noreply.github.com>
2021-05-09 14:21:39 +0530

    Add files via upload

---
+++ Mail_Extractor.py
@@ -0,0 +1,117 @@
+from tkinter import *
+import re
+
+top = Tk()
+top.geometry('300x300')
+
+def OPEN():
+    f2=open('tk.txt', 'w')
+    f2.write('Put some text below: ')
+    f2.close()
+
+def abc(str):
+    email = re.findall(r"[0-9a-zA-Z._+%]+@[0-9a-zA-Z._+%]+[.][a-zA-Z.0-9]+", str)
+    for i,j in enumerate(email):
+        print(f'Mail({i+1}) -----> {j}')
+
+def GET():
+    f = open('tk.txt', 'r')
+    str1 = f.read()
+    abc(str1)
+
+st = StringVar()
+def SUBMIT():
+    str2 = st.get()
+    abc(str2)
+
+Open = Button(top, text='Open txt file', command=OPEN).place(x=30, y=40)
+Get = Button(top, text='Get Email', command=GET).place(x=200, y=40)
+
+for_text = Label(top, text='Paste text here').place(x=90, y=80)
+e1 = Entry(top,textvariable=st).place(x=10, y=100, width= 280, height=150)
+Submit = Button(top, text='Submit', command=SUBMIT).place(x=120, y=260)
+top.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # import tkinter as tk
+# from tkinter import *
+# import re
+# top = Tk()
+# top.geometry('200x100')
+
+# def b1():
+#     f = open('regex_py.txt', 'r')
+#     str = f.read()
+#     email = re.findall(r"[0-9a-zA-Z._+%]+@[0-9a-zA-Z._+%]+[.][a-zA-Z.0-9]+", str)
+#     print(email)
+#     f.close()
+
+# submit = Button(top, text='Get mail', command=b1).place(x=70, y=40)
+# top.mainloop()
+
+
+
+
+'''
+from tkinter import *
+import re
+top = Tk()
+top.geometry('300x300')
+
+def OPEN():
+    f2=open('tk.txt', 'w')
+    f2.write('Put some text below: ')
+    f2.close()
+
+def abc(str):
+    email = re.findall(r"[0-9a-zA-Z._+%]+@[0-9a-zA-Z._+%]+[.][a-zA-Z.0-9]+", str)
+    # print(email)
+    for i,j in enumerate(email):
+        print(f'{i} -----> {j}')
+
+def GET():
+    f = open('tk.txt', 'r')
+    str1 = f.read()
+    abc(str1)
+    # email = re.findall(r"[0-9a-zA-Z._+%]+@[0-9a-zA-Z._+%]+[.][a-zA-Z.0-9]+", str)
+    # # print(email)
+    # for i,j in enumerate(email):
+    #     print(f'{i} --> {j}')
+    # f.close()
+
+st = StringVar()
+def SUBMIT():
+    str2 = st.get()
+    abc(str2)
+    # str1 = st.get()
+    # email = re.findall(r"[0-9a-zA-Z._+%]+@[0-9a-zA-Z._+%]+[.][a-zA-Z.0-9]+", str1)
+    # # print(email)
+    # for i,j in enumerate(email):
+    #     print(f'{i} -----> {j}')
+
+
+Open = Button(top, text='Open txt file', command=OPEN).place(x=30, y=40)
+Get = Button(top, text='Get Email', command=GET).place(x=200, y=40)
+
+for_text = Label(top, text='Paste text here').place(x=90, y=80)
+e1 = Entry(top,textvariable=st).place(x=10, y=100, width= 280, height=150)
+Submit = Button(top, text='Submit', command=SUBMIT).place(x=120, y=260)
+# Submit.pack(fill='x', expand=True)
+
+top.mainloop()'''
+
+
+
+
+
